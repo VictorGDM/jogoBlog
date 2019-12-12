77bdbbc8590154a88b687dcd57f5277b31050bbd
Victor-01 <vgdiasmachado@gmail.com>
2019-12-12 00:01:49 -0200

    Organizando documentacao

--- resources/views/documentacao/index.blade.php
+++ resources/views/documentacao/index.blade.php
@@ -4,7 +4,108 @@
 <link href="{{asset('sass/documentacao.scss')}}" rel="stylesheet">
 
 @foreach ($documentacoes as $documentacao)
-<label for="aa">{{$documentacao}}</label>
-    
+
+<div id="gameDesign" class="row">
+    <div class="col-md-3 colmd1-1">
+        <div id="card1-1" class="card">
+            <img class="imgGD" src="{{asset('imgs/y.png')}}">
+        </div>
+    </div>
+
+    <div class="col-md-8 colmd1-2">
+        <div id="card1-2" class="card">
+            <div class="card-body">
+                <h5 class="card-title">Game Design</h5>
+
+                <div class="perfilUsuario">
+                    <h6 class="card-subtitle mb-2 text-muted">Perfil de Usuário</h6>
+                    <p class="card-text">{{$documentacao->perfilUsuario}}</p>
+                </div>
+
+                <div class="aplicacao">
+                    <h6 class="card-subtitle mb-2 text-muted">Aplicação</h6>
+                    <p class="card-text">{{$documentacao->aplicacao}}</p>
+                </div>
+
+                <div class="plataforma">
+                    <h6 class="card-subtitle mb-2 text-muted">Plataforma</h6>
+                    <p class="card-text">{{$documentacao->plataforma}}</p>
+                </div>
+
+                <div class="ritmo">
+                    <h6 class="card-subtitle mb-2 text-muted">Ritmo</h6>
+                    <p class="card-text">{{$documentacao->ritmo}}</p>
+                </div>
+
+                <div class="modoJogo">
+                    <h6 class="card-subtitle mb-2 text-muted">Modo de Jogo</h6>
+                    <p class="card-text">{{$documentacao->modoJogo}}</p>
+                </div>
+
+                <div class="genero">
+                    <h6 class="card-subtitle mb-2 text-muted">Gênero</h6>
+                    <p class="card-text">{{$documentacao->genero}}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="nivJogMus" class="row teste">
+    <div class="col-md-8 colmd2-1">
+        <div id="card2-2" class="card">
+            <div class="card-body">
+                <h5 class="card-title">Jogabilidade</h5>
+                <div class="jogabilidade">
+                    <p class="card-text">{{$documentacao->jogabilidade}}</p>
+                </div>
+
+                <div class="niveis">
+                    <h6 class="card-subtitle mb-2 text-muted">Níveis</h6>
+                    <p class="card-text">{{$documentacao->niveis}}</p>
+                </div>
+
+                <div class="musica">
+                    <h6 class="card-subtitle mb-2 text-muted">Musicalização e Sonoplastia</h6>
+                    <p class="card-text">{{$documentacao->musiSono}}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-3 colmd2-1">
+        <div id="card2-1" class="card">
+            <img class="imgJB" src="{{asset('imgs/cg.png')}}">
+        </div>
+    </div>
+</div>
+
+<div id="narrativa" class="row teste2">
+    <div class="col-md-3 colmd3-1">
+        <div id="card3-1" class="card">
+            <img class="imgN" src="{{asset('imgs/p.png')}}">
+        </div>
+    </div>
+
+    <div class="col-md-8 colmd3-2">
+        <div id="card3-2" class="card">
+            <div class="card-body">
+                <h5 class="card-title">Narrativa</h5>
+
+                <div class="narrativa">
+                    <p class="card-text">{{$documentacao->narrativa}}</p>
+                </div>
+
+                <div class="personagens">
+                    <h6 class="card-subtitle mb-2 text-muted">Personagens</h6>
+                    <p class="card-text">-Mario</p>
+                    <p class="card-text">-Luigi</p>
+                    <p class="card-text">-Peach</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endforeach
+
 @endsection
--- resources/views/forum/create.blade.php
+++ resources/views/forum/create.blade.php
@@ -1,79 +1,82 @@
 @extends('template.app')
 
 @section('content')
+<link href="{{asset('sass/criar.scss')}}" rel="stylesheet">
 
-<div class="col-md-12">
-    <h3>Novo Fórum</h3>
-</div>
+<div class="criar">
+    <div class="col-md-12">
+        <h3>Novo Fórum</h3>
+    </div>
 
-<div class="col-md-8 well">
-    <form class="col-md-12" action="{{ url('forum/store')}}" method="POST">
-        {{ csrf_field() }}
-        <div class="row">
-            <div class="form-group col-md-10">
-                <label class="control-label" for="tituloFor">Título</label>
-                <input type="text" name="tituloFor" class="form-control">
-            </div>
-        </div>
-
-        <div class="form-group row">
-            <div class="form-check col-md-4">
-                <input class="form-check-input" type="radio" name="tipo" id="gridRadios1" value="Avaliação">
-                <label class="form-check-label" for="gridRadios1">
-                    <i class="fas fa-check"></i>
-                    Avaliação
-                </label>
+    <div class="col-md-8 well">
+        <form class="col-md-12" action="{{ url('forum/store')}}" method="POST">
+            {{ csrf_field() }}
+            <div class="row">
+                <div class="form-group col-md-10">
+                    <label class="control-label" for="tituloFor">Título</label>
+                    <input type="text" name="tituloFor" class="form-control">
+                </div>
             </div>
 
-            <div class="form-check col-md-4">
-                <input class="form-check-input" type="radio" name="tipo" id="gridRadios2" value="Bug">
-                <label class="form-check-label" for="gridRadios2">
-                    <i class="fas fa-bug"></i>
-                    Bug
-                </label>
+            <div class="form-group row">
+                <div class="form-check col-md-4">
+                    <input class="form-check-input" type="radio" name="tipo" id="gridRadios1" value="Avaliação">
+                    <label class="form-check-label" for="gridRadios1">
+                        <i class="fas fa-check"></i>
+                        Avaliação
+                    </label>
+                </div>
+
+                <div class="form-check col-md-4">
+                    <input class="form-check-input" type="radio" name="tipo" id="gridRadios2" value="Bug">
+                    <label class="form-check-label" for="gridRadios2">
+                        <i class="fas fa-bug"></i>
+                        Bug
+                    </label>
+                </div>
+
+                <div class="form-check col-md-4">
+                    <input class="form-check-input" type="radio" name="tipo" id="gridRadios3" value="Erro">
+                    <label class="form-check-label" for="gridRadios2">
+                        <i class="fas fa-exclamation-triangle"></i>
+                        Erro
+                    </label>
+                </div>
+
+                <div class="form-check col-md-4">
+                    <input class="form-check-input" type="radio" name="tipo" id="gridRadios4" value="Reclamação">
+                    <label class="form-check-label" for="gridRadios2">
+                        <i class="fas fa-frown"></i>
+                        Reclamação
+                    </label>
+                </div>
+
+                <div class="form-check col-md-4">
+                    <input class="form-check-input" type="radio" name="tipo" id="gridRadios5" value="Dúvida">
+                    <label class="form-check-label" for="gridRadios2">
+                        <i class="fas fa-question"></i>
+                        Dúvida
+                    </label>
+                </div>
+
+                <div class="form-check col-md-4">
+                    <input class="form-check-input" type="radio" name="tipo" id="gridRadios6" value="Outros" checked>
+                    <label class="form-check-label" for="gridRadios3">
+                        <i class="fas fa-ellipsis-h"></i>
+                        Outros
+                    </label>
+                </div>
             </div>
 
-            <div class="form-check col-md-4">
-                <input class="form-check-input" type="radio" name="tipo" id="gridRadios3" value="Erro">
-                <label class="form-check-label" for="gridRadios2">
-                    <i class="fas fa-exclamation-triangle"></i>
-                    Erro
-                </label>
+            <div class="row">
+                <div class="form-group col-md-12">
+                    <label class="control-label" for="descricaoFor">Conteudo</label>
+                    <textarea type="text" name="descricaoFor" class="form-control col-md-12"></textarea>
+                </div>
             </div>
-
-            <div class="form-check col-md-4">
-                <input class="form-check-input" type="radio" name="tipo" id="gridRadios4" value="Reclamação">
-                <label class="form-check-label" for="gridRadios2">
-                    <i class="fas fa-frown"></i>
-                    Reclamação
-                </label>
-            </div>
-
-            <div class="form-check col-md-4">
-                <input class="form-check-input" type="radio" name="tipo" id="gridRadios5" value="Dúvida">
-                <label class="form-check-label" for="gridRadios2">
-                    <i class="fas fa-question"></i>
-                    Dúvida
-                </label>
-            </div>
-
-            <div class="form-check col-md-4">
-                <input class="form-check-input" type="radio" name="tipo" id="gridRadios6" value="Outros" checked>
-                <label class="form-check-label" for="gridRadios3">
-                    <i class="fas fa-ellipsis-h"></i>
-                    Outros
-                </label>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="form-group col-md-12">
-                <label class="control-label" for="descricaoFor">Conteudo</label>
-                <textarea type="text" name="descricaoFor" class="form-control col-md-12"></textarea>
-            </div>
-        </div>
-        <button class="btn btn-primary" type="submit">Salvar</button>
-    </form>
+            <button class="btn btn-primary" type="submit">Salvar</button>
+        </form>
+    </div>
 </div>
 
 @endsection
